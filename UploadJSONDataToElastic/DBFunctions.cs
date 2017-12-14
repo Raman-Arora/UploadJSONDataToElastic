18ed09b4af18d548b9add1a6e38a339ceb9a905d
Vanraj Narula <vanrajbbsbec@gmail.com>
2017-12-14 15:44:52 +0530

    AddedDefault function for DBFunction class

---
+++ UploadJSONDataToElastic/DBFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace UploadJSONDataToElastic
+{
+    public class DBFunctions
+    {
+        public DataTable getResultSetFromtext(string qeury, string connectionString)
+        {
+            DataTable clientResult = null;
+            try
+            {
+                
+            }
+            catch(Exception ex)
+            {
+                //Log The exception
+            }
+            return clientResult;
+        }
+
+        public DataTable getResultSetFromProc(string procName, string connectionString)
+        {
+            DataTable clientResult = null;
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                //Log the execption
+            }
+            return clientResult;
+        }
+
+    }
+}

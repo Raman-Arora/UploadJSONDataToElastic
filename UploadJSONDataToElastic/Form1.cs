6c9f7ec6a51f531978857b6d52e143270230edae
Raman Arora <ramankumar.sviet@gmail.com>
2017-12-14 15:03:41 +0530

    Added Connection String

--- UploadJSONDataToElastic/Form1.cs
+++ UploadJSONDataToElastic/Form1.cs
@@ -17,5 +17,28 @@ namespace UploadJSONDataToElastic
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string serverIP = txtServerIP.Text;
+            string password = txtServerPwd.Text;
+            string dbName = txtDatabaseName.Text;
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=" + serverIP + ";Database=" + dbName + ";Trusted_Connection=true";
+                // using the code here...
+            }
+
+        }
+
+     
+       
     }
 }
